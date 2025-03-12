e585cafda87e59a9176471b8718820873c6996f4
Burhan <burhanyldz@gmail.com>
2025-03-12 11:21:24 +0300

    Add option to toggle clear buttons in VectorPen initialization

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const vectorPen = new VectorPen({
     showToolbar: true,
     toolbarPosition: 'top', // Can be: 'left', 'right', 'top', 'bottom', 'top-left', 'top-right', 'bottom-left', 'bottom-right'
-    toolbarContainer: document.body
+    toolbarContainer: document.body,
+    showClearButtons: false
   });
   
   // Tüm çizim hedefi elemanlarına ekle
